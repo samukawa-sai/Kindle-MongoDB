c43394250721576c70d2c22ca302727cac989f5a
root <root@DESKTOP-IDHHK0S>
2024-08-15 16:44:06 +0900

    3

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express=require("express")
 const app = express()
 
+app.get("/",(req,res)=>{
+    res.send("こんにちは")
+})
+
+app.post("/blog/create",(req,res)=>{
+    console.log("POSTリクエストが実行されました")
+})
+
 app.listen(5000,()=>{
     console.log("Listening on localhost port 5000")
 })
