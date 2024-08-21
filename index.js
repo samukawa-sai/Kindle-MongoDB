8374562f49cf7367eafb68508b77a25242b3fa46
samukawa-sai <sekaino315@gmail.com>
2024-08-21 17:01:08 +0900

    kagato

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express=require("express")
 const app = express()
+app.use(express.urlencoded({extended:true}))
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb+srv://samukawa:world315@cluster0.zzh2r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        console.log("Success: Connected to MongoDB")
+    })
+    .catch((error) => {
+        console.error("Failure: Unconnected to MongoDB")
+    })
+
+const Schema = mongoose.Schema
+const BlogSchema = new Schema({
+    title:String,
+    summary:String,
+    image:String,
+    textBody:String,
+})
+
+const BlogModel = mongoose.model("Blog",BlogSchema)
 
 app.get("/",(req,res)=>{
     res.send("こんにちは")
 })
 
-app.post("/blog/create",(req,res)=>{
+app.get("/blog/create",(req,res)=>{
+    res.sendFile(__dirname+"/views/blogCreate.html")
+})
+
+app.post("/blog/create",async (req,res)=>{
     console.log("POSTリクエストが実行されました")
+    try{
+        const savedBlogData = await BlogModel.create(req.body);
+        console.log("成功",savedBlogData);
+        res.send("投稿完了")
+    }catch(err){
+        console.log("失敗",err);
+        res.status(500).send("エラーの発生");
+    }
 })
 
 app.listen(5000,()=>{
